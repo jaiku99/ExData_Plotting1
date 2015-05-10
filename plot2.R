ac8ac21394189f83bcc1b050a22585680d7a11b0
Jai Ramachandran <jai.home@gmail.com>
2015-05-10 10:42:02 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,30 @@
+# Script to generate Global Active Power - Plot 2 
+
+setwd( "./Coursera/data")
+
+XPower <- read.table("household_power_consumption.txt", header = TRUE, sep =";", 
+                     quote="",  na.string="?")
+
+library(dplyr)
+
+XPower <- mutate(XPower, FDate= as.Date(Date, format="%d/%m/%Y"))
+
+XPower <-  filter(XPower, (FDate =="2007-02-01" | FDate =="2007-02-02") )
+
+
+Date_Time <- strptime(paste(XPower$FDate, XPower$Time), "%Y-%m-%d %H:%M:%S")
+Gpower <- cbind(Gpower, Date_Time)
+
+windows()
+#plot1 
+
+png(file= "plot2.png")
+
+
+#plot2
+windows()
+with( XPower, plot(Date_Time, Global_active_power, type ="l", 
+                   lty="solid", xlab= " ", ylab ="Global Active power(kilowatts)"))
+
+dev.copy(png, file= "plot2.png")
+dev.off()
